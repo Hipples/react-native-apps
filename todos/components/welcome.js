f2cf73d23c145d7a495eb3b909f788edd89a60ff
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-17 16:58:39 -0700

    created welcome component outline

---
+++ todos/components/welcome.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const Welcome = () => (
+    <View style={styles.container}>
+        <Text style={styles.text}>Welcome</Text>
+    </View>
+);
+
+const styles = ({
+    container: {},
+    text: {}
+});
+
+export default Welcome;
