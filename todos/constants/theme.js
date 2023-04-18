b3288eda3e8af5c4896c5dd869c30c9cd8c2ba08
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-17 17:44:25 -0700

    will this update the snack?

---
+++ todos/constants/theme.js
@@ -0,0 +1,20 @@
+const colors = {
+    primary: '#F5F5F5',     //off-white
+    secondary: '#EDEDED',  //gray
+    tertiary: '#AF2F2F',  //pastel pink --> (opacity=0.2)
+
+    white: '#FFFFFF',
+    black: '#000000',
+}
+
+const sizes = {
+    xxsmall: 5,
+    xsmall: 10,
+    small: 15,
+    medium: 20,
+    large: 25,
+    xlarge: 30,
+    xxlarge: 35,
+
+    pageHeading: 75
+}
