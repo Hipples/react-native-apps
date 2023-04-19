a3c759e1b4c203a3db584a3463a29c4d999bf45c
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-04-18 21:57:39 -0700

    something on notes?

---
+++ todos/components/common/submit-button.js
@@ -0,0 +1,13 @@
+import { View, Text, Pressable } from 'react-native';
+
+const SubmitButton = () => (
+    <View>
+        <Pressable>
+            <Text>
+
+            </Text>
+        </Pressable>
+    </View>
+);
+
+export default SubmitButton;
